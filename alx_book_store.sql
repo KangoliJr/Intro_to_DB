80e7b4e1f5d263f5761b0f6aeeda6fb0f984ba94
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-07-05 16:14:41 +0300

    revised

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 -- Create Authors table
 CREATE TABLE IF NOT EXISTS `Authors` (
-    `author_id` INT PRIMARY KEY AUTO_INCREMENT,
-    `author_name` VARCHAR(215) NOT NULL
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    author_name VARCHAR(215) NOT NULL
 );
 
 -- Create Books table
